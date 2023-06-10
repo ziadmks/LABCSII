573d7b61ad3bf9e788a2a24e680619e24e6751eb
ziadmks <Ziadmk@aucegypt.edu>
2023-06-10 22:59:47 +0300

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "fun.h"
+using namespace std; 
+
+int main()
+{
+int val[] = { 60, 100, 120 };
+int wt[] = { 10, 20, 30 };
+int W = 50;
+int n = sizeof(val) / sizeof(val[0]);
+cout << knapSack(W, wt, val, n);
+return 0;
+}
